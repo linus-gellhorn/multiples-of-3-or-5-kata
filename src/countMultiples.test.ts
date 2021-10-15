e83d32e8efcd24d9ee84923ccb30996853d31547
Linus Gellhorn <linus.g@hotmail.com>
2021-10-15 16:48:56 +0100

    Formatted with Prettier

--- src/countMultiples.test.ts
+++ src/countMultiples.test.ts
@@ -1,7 +1,7 @@
 import countMultiples from "./countMultiples";
 
 test("countMultiples counts the number of multiples of 3 and 5 up to a given number", () => {
-    expect(countMultiples(10)).toBe(23);
-    expect(countMultiples(16)).toBe(60);
-    expect(countMultiples(-5)).toBe(0);
+  expect(countMultiples(10)).toBe(23);
+  expect(countMultiples(16)).toBe(60);
+  expect(countMultiples(-5)).toBe(0);
 });
--- src/countMultiples.ts
+++ src/countMultiples.ts
@@ -1,4 +1,4 @@
-// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 // The sum of these multiples is 23.
 
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number
@@ -8,14 +8,14 @@
 
 function countMultiples(number: number): number {
   let count = 0;
-  for (let i=0; i < number; i++) {
+  for (let i = 0; i < number; i++) {
     if (i % 3 === 0) {
       count += i;
     } else if (i % 5 === 0) {
       count += i;
     }
   }
-  return count 
+  return count;
 }
 
 export default countMultiples;
